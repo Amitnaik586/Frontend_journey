385d52610a44c0d14c5912f6e758ae7046a8d9e6
Amit Naik <amitnaik6363@gmail.com>
2026-08-19 18:52:34 +0530

    Create script.js with variable examples in JavaScript
    Added a script.js file demonstrating variable declarations and scopes in JavaScript.

---
+++ Month-2-Javascript/day-1/script.js
@@ -0,0 +1,47 @@
+// varibales in javascript //
+
+//1.var keyword in javascript// 
+
+var name = 'Amit';
+
+//2.let keyword in javascript//
+
+let age = 19;
+
+//3.const keyword in javascript//
+
+const course = 'BCA';
+
+//Scope in javascript//
+
+var his_name  = 'Bob';
+function printName () {
+    console.log(his_name);   // this varible is globaly scoped //
+}
+
+printName()
+
+//function scoped //
+
+
+function printyear () {
+    var year = 2020;     // this variable is function scoped and we get a error because of function varible //
+}
+
+console.log(year);
+
+
+//using const keyword in javascript//
+
+let name = "amit ramadas naik"
+
+
+
+
+
+
+
+
+
+
+
