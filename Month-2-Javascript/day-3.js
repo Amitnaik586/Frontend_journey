a94c4804c29de624e29988a6d8f244411179e51e
Amit Naik <amitnaik6363@gmail.com>
2026-08-22 20:01:34 +0530

    Add data types summary in day-3.js
    Added a summary of data types in JavaScript, including examples of primitive and reference types.

---
+++ Month-2-Javascript/day-3.js
@@ -0,0 +1,32 @@
+//DATA TYPES SUMMARY
+
+// Primitive 
+
+// 7 types : string, num, boolean, null, undefined, symbol, BigInt
+//examples
+let string = "Amit";
+let num = 25;
+let boolean = true;
+
+
+
+
+
+// reference (Non primitive)
+
+// Array, Objects, Functions
+//examples
+
+const Animlas = ["Dog", "Cat", "Lion", "Tiger"];
+
+let MyOBject = {
+    name : "Amit",
+    age : 19,
+    course : "BCA"
+}
+
+let MyFunction = function () {
+    console.log("hello world");
+    
+}
+
