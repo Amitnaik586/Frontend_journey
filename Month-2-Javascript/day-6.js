d7fc0520f5c013c310197634514a2a2c01d8eb86
Amit Naik <amitnaik6363@gmail.com>
2026-08-23 20:26:06 +0530

    Add JavaScript array methods example

---
+++ Month-2-Javascript/day-6.js
@@ -0,0 +1,19 @@
+// Array in javascript
+
+//array declaration
+let myArray =["amit",19,'BCA'];
+console.log(myArray);
+
+//some array methods
+myArray.push("2006");
+console.log(myArray);
+
+myArray.pop();
+console.log(myArray);
+
+myArray.unshift("dharwadUniversity");
+console.log(myArray);
+console.log(myArray.includes("amit"))
+console.log(myArray.includes("darshan"));
+
+
