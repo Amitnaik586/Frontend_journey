8d1a2e977d0a7dcfb25580bb653145be2a74e50c
Amit Naik <amitnaik6363@gmail.com>
2026-08-23 18:02:02 +0530

    Add string methods examples in day-5.js
    Add string manipulation examples demonstrating various methods.

---
+++ Month-2-Javascript/day-5.js
@@ -0,0 +1,18 @@
+//Strings 
+let string = "AmitNaik";
+let name = 'Amit';
+
+//Some String Methods
+console.log(string[3]);
+console.log(string.charAt(1));
+console.log(string.length);
+console.log(string.toUpperCase());
+console.log(string.toLowerCase())
+console.log(string.indexOf('k'));
+console.log(string.substring(0,4));
+console.log(string.slice(4,8));
+console.log(string.trim());
+console.log(string.replace('Naik', '__'))
+
+
+
