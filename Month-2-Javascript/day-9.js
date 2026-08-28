09cb6501d53136f0be595b3be877600780b2bd51
Amit Naik <amitnaik6363@gmail.com>
2026-08-28 18:55:26 +0530

    Add functions for logging and summing numbers

---
+++ Month-2-Javascript/day-9.js
@@ -0,0 +1,18 @@
+// Function in Javascript
+
+// function declaration 
+function myFunction () {
+    console.log("hello wellcome");
+    
+}
+
+// function calling 
+myFunction()
+
+// function with parameter
+
+function sum (num1, num2) {
+    console.log(num1+num2);
+    
+}
+sum(10, 20)
