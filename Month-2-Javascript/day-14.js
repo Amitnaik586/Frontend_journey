a4642578d205e6b3ff5cdf21f9888ecddd8919a8
Amit Naik <amitnaik6363@gmail.com>
2026-09-01 20:55:11 +0530

    Implement switch statement example in day-14.js
    Add example of switch statement in JavaScript

---
+++ Month-2-Javascript/day-14.js
@@ -0,0 +1,36 @@
+// switch in javascript
+
+// switch decleration in javscript
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+// example of switch 
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("feb");
+        break;
+
+    case 3:
+        console.log("mar");
+        break;
+        
+
+    default:
+        console.log("non of these");
+        
+        break;
+}
+
