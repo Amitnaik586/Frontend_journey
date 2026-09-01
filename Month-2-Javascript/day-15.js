a82d606e99147eb05a14c23e3f06f41c40b9f17a
Amit Naik <amitnaik6363@gmail.com>
2026-09-01 20:58:02 +0530

    Document truthy and falsy values in day-15.js
    Add comments explaining truthy and falsy values in JavaScript.

---
+++ Month-2-Javascript/day-15.js
@@ -0,0 +1,23 @@
+// Truthy values and Flasy values
+
+// flasy values
+//  false
+//  0
+//  -0
+//  BigInt 0n
+//  ""
+//  null
+//  undefined
+//  NaN
+
+
+
+
+// truthy values
+// "0"
+// "false"
+// " "
+//[]
+//{}
+//function () {}
+
