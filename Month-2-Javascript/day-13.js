f0c4cf97da89d4d7dcc71570c491772fe579c8e0
Amit Naik <amitnaik6363@gmail.com>
2026-09-01 11:47:40 +0530

    Implement control flow examples in day-13.js
    Added control flow examples using if statements and nested ifs.

---
+++ Month-2-Javascript/day-13.js
@@ -0,0 +1,66 @@
+// Control flow in Javascript
+
+
+// if statement decleration
+if (condition){
+    console.log("hello world ");     // decleration
+    
+}
+
+
+// example of if statement
+const isUserLoggedIn = "yes"
+if (isUserLoggedIn == "yes") {           // if this condition is true javascript runs this block of code
+    console.log("Yes, User logged in");
+}
+
+else{
+    console.log("user not logged in");   // if the condition is false this block of code will run
+    
+}
+
+// Nested if
+let balance = 650
+
+if ( balance < 500) {
+    console.log("less then 500");
+    
+}
+else if (balance < 750)    {          
+    console.log("less then 750");
+    
+}
+else if ( balance < 900){
+    console.log("less then 900");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
