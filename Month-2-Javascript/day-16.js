37d0166894f1a038c9deb62eb6a873d93a78f391
Amit Naik <amitnaik6363@gmail.com>
2026-09-03 20:14:36 +0530

    Add for loop examples in day-16.js
    Added examples of for loops demonstrating break, continue, and multiplication tables.

---
+++ Month-2-Javascript/day-16.js
@@ -0,0 +1,24 @@
+// for loop with break and continue
+
+// for loop decleration
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+
+}
+
+// example of for loop
+for (let index = 0; index < 11; index++) {
+    const element = index;
+    console.log(element);
+    
+    
+}
+
+// multyple table example
+let num = 2;
+for  ( let i = 1; i<=11; i++) {
+    console.log(num,"x",i,"=",num*i);
+   
+    
+}
