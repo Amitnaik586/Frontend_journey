cd2598dec6323cac3fd29cffa15900a8a7585c6b
Amit Naik <amitnaik6363@gmail.com>
2026-09-04 10:32:07 +0530

    Implement while and do while loop examples
    Added examples of while and do while loops in JavaScript.

---
+++ Month-2-Javascript/day-18.js
@@ -0,0 +1,21 @@
+// while and do while loop
+
+// while loop decleration
+while (condition) {
+    //code
+}
+
+// example for while loop
+let num = 22;
+let i = 1;
+while (i<=11) {
+    console.log(num,"x",i,"=",num*i);
+    i++
+}
+
+// do while loop decleration
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++
+}while(score <= 10)   
