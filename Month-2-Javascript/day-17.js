01a0e4b2fa0c032b32e33bc43dbe89e74da22e05
Amit Naik <amitnaik6363@gmail.com>
2026-09-04 10:31:35 +0530

    Add for...in loop example in day-17.js
    Add example of for...in loop in JavaScript.

---
+++ Month-2-Javascript/day-17.js
@@ -0,0 +1,16 @@
+// for...in loop
+for (propety in object) {
+    // code
+}
+
+// for..in loop example
+let arr = [23,56,78,90,45,68,57];
+for (let num in arr )   {
+    if ( num == 5) {
+        break;
+        
+
+    }
+    console.log(num);
+}
+
